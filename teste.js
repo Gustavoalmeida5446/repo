0228e57ed33d04ea6d2ec2d2df9e21b0801a85e0
Gustavoalmeida5446 <110276048+Gustavoalmeida5446@users.noreply.github.com>
2024-10-03 15:40:18 -0300

    valid password challange

--- teste.js
+++ teste.js
@@ -1,3 +1,11 @@
-const kohlerName = ['colera', 'cooler', 'pedrao', 'pedrao das ideias', 'pedro', 'grande'];
-const nick = Math.floor(Math.random()*kohlerName.length);
-alert (`Fala ${kohlerName[nick]}`);
+// const kohlerName = ['colera', 'cooler', 'pedrao', 'pedrao das ideias', 'pedro', 'grande'];
+// const nick = Math.floor(Math.random()*kohlerName.length);
+// alert (`Fala ${kohlerName[nick]}`);
+
+
+function validPassword (password, username){
+const size = password.length >8;
+const space = password.indexOf(' ') !== -1;
+const containsUsername = password.indexOf(username) !== -1;
+return !size && !space && !containsusername;
+}
