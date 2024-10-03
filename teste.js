81f12e90d0a602c53e06fa584c4c8108fa28a907
Gustavoalmeida5446 <110276048+Gustavoalmeida5446@users.noreply.github.com>
2024-10-03 15:49:07 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -3,9 +3,9 @@
 // alert (`Fala ${kohlerName[nick]}`);
 
 
-function validPassword (password, username) {
-const size = password.length < 8;
-const space = password.indexOf(' ') !== -1;
-const name = password.indexOf(username) !== -1;
-return !size && !space && !name;
+function validPassword(password, username) {
+  const size = password.length >= 8; // Verifica se o comprimento da senha é maior ou igual a 8
+  const space = password.indexOf(' ') === -1; // Verifica se não há espaço na senha
+  const name = password.indexOf(username) === -1; // Verifica se o nome de usuário não está na senha
+  return size && space && name; // Retorna true se todos os critérios forem satisfeitos
 }
