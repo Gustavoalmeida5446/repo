b82cc48b0367ac04982271e3972d6a5793b850ed
Gustavoalmeida5446 <110276048+Gustavoalmeida5446@users.noreply.github.com>
2024-10-07 14:40:22 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -3,16 +3,74 @@
 // alert (`Fala ${kohlerName[nick]}`);
 
 
-// function validPassword(password, username) {
-//   const size = password.length >= 8; // se a senha for maior que 9 caracteres, true
-//   const space = password.indexOf(' ') === -1; // se não tiver espaço na senha, true
-//   const name = password.indexOf(username) === -1; // se não tiver o username na senha, true
-//   return size && space && name; // se alguma variável não for true, retorna false
+function validPassword(password, username) {
+    const size = password.length >= 8; // se a senha for maior que 9 caracteres, true
+    const space = password.indexOf(' ') === -1; // se não tiver espaço na senha, true
+    const name = password.indexOf(username) === -1; // se não tiver o username na senha, true
+    return size && space && name; // se alguma variável não for true, retorna false
+}
+
+// function validPassword (password, username) {
+//     const size = password.length < 8;
+//     const space = password.indexOf(' ') !== -1;
+//     const name = password.indexOf(username) !== -1;
+//     return !size && !space && !name;
+//     }
+
+// function avg(arr){
+//     let total = 0;
+//     for (let num of arr) {
+//         total = total + num;
+//         console.log("🚀 ~ avg ~ total:", total)
+//     }
+//    return total/arr.length;
 // }
 
-function validPassword (password, username) {
-const size = password.length < 8;
-const space = password.indexOf(' ') !== -1;
-const name = password.indexOf(username) !== -1;
-return !size && !space && !name;
+// const arr = [200, 310, 400, 500]
+// let total = 0;
+// let i = 0;
+// // for(let i = 0; i < arr.length && total < 500 ; i = i+1){
+// for(; i < arr.length && total < 500; i++){
+//     total += arr[i]
+    
+// }
+// console.log("🚀 ~ i:", i)
+// console.log("🚀 ~ total:", total)
+
+
+// const arr = [1, 2, 3, 4]
+// let total = 0;
+// function avg(arr){
+// for (let i = 0; i < arr.length && total += i; i++){
+
+//     console.log(i);
+// }
+// }
+
+// const arr = [1, 2, 3, 4]
+// let total = 0;
+// function avg(arr){
+//     for(let num of arr)
+//         total = total + num;
+//     console.log(total/arr.length);
+//     }
+
+
+function average(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total / arr.length;
+}
+
+const sentence = 'ninguem acertou o horario exato no blog ruim que pedro fez em javascript';
+function pangram(sentence) {
+    let lowercased = sentence.toLowerCase();
+    for (let char of 'abcdefghijlmnopqrstuvxzkwy') {
+        if (lowercased.indexOf(char) === -1) {
+            return false;
+        }
+    }
+    return true;
 }
